9bdf5a0e1537b6d98757bfb2c7cf7c00de7f242f
AugustoLuque <augustoluque.sec@institutolamerced.edu.ar>
2026-09-13 01:36:40 -0300

    act

--- docs/assets/insm-tools.js
+++ docs/assets/insm-tools.js
@@ -12,7 +12,7 @@
 	   como Aplicación Web (ver instrucciones dentro de ese archivo). Este es
 	   el ÚNICO lugar del sitio que hay que tocar para conectar los formularios
 	   a la planilla de Google Sheets. */
-	var APPS_SCRIPT_ENDPOINT = 'https://script.google.com/macros/s/AKfycbwOV0FldYvJj1WGMr7xUGHzBJcdqhA_v8TmvkgeaPkeADFMnayQbPIQQbFBhRihUCuP/exec';
+	var APPS_SCRIPT_ENDPOINT = 'https://script.google.com/macros/s/AKfycbzJ6U2Rcy2cTkB6K_yI-aRyJbR6oVIIyFsXg7YB31HPQhRSLt_bCrWZqMhRhUbi8xje/exec';
 
 	/* -------------------- 1) Directorio de lugares del sitio -------------------- */
 	var SITE_DIRECTORY = [
